952adc61adf22229aa9f88ae4d1101a714f9d451
a <109896751+agrikatheprogrammer@users.noreply.github.com>
2025-05-05 23:23:28 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
-import whisper,shutil,os,uuid
+import whisper,shutil,os,uuid,re
 app=FastAPI()
 model=whisper.load_model('base')
 @app.post("/transcribe")
@@ -8,6 +8,6 @@ async def transcribe_audio(file:UploadFile=File(...)):
     tempfilename=f"temp_{uuid.uuid4()}.{file_ext}"
     with open(tempfilename,'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
-    result=model.transcribe(tempfilename)
+    result=model.transcribe(tempfilename, language='es')
     os.remove(tempfilename)
-    return { "text": result["text"].trim() }
+    return { "text": re.sub(r'[^A-Za-z0-9]','',result["text"]).strip() }
